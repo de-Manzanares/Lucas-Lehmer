fedfa6bda85c6cbb9d6a154497969fa4289cccbc
de-Manzanares <138251011+de-Manzanares@users.noreply.github.com>
2023-10-01 20:14:30 -0700

    testing modulo algorithm
    Method 1 uses the "straight" mod function
    Method 2 uses the Mersenne modulo algorithm

--- main.cpp
+++ main.cpp
@@ -14,12 +14,10 @@ int main()
     // cout << "p = ";
     // if ((cin >> p)&&(p>1)) {
 
-    int const mk = 28;
+    int const mk = 20;
     int const tk = 5;
 
-    int test_mp[mk] = {2, 3, 5, 7, 13, 17, 19, 31, 61, 89, 107, 127, 521, 607,
-            1279, 2203, 2281, 3217, 4253, 4423, 9689, 9941, 11213, 19937, 21701,
-            23209, 44497, 86243};//, 110503, 132049, 216091};
+    int test_mp[mk] = {2, 3, 5, 7, 13, 17, 19, 31, 61, 89, 107, 127, 521, 607,1279, 2203, 2281, 3217, 4253, 4423};// 9689, 9941, 11213, 19937, 21701, 23209, 44497, 86243};//, 110503, 132049, 216091};
     int results_Method_1[mk][tk] = {0};
     int results_Method_2[mk][tk] = {0};
 
@@ -99,15 +97,73 @@ int main()
 
     for (int i=0; i<mk; i++) {
         for (int j=0; j<tk; j++){
+            auto start_time = chrono::high_resolution_clock::now();
+            mpz_t k;
+            mpz_t l;
+            mpz_t m;
+            mpz_t Mp;
+            mpz_t Mp_2n;
+            int count = 1;
+            unsigned long int limit = p-2;
+            mpz_init_set_ui(k, 4);
+            mpz_init(l);
+            mpz_init(m);
+            mpz_init(Mp);
+            mpz_init(Mp_2n);
+            mpz_ui_pow_ui(Mp_2n, 2, p);
+            mpz_sub_ui(Mp, Mp_2n, 1);
 
+            cout << "\nu(0) = 4\n" << endl;
+
+            do {
+
+                cout << "u(" << count << ") = (" << k << " ^ 2 - 2) % M_" << p;
+                cout << " = ";
+
+                mpz_mul(k, k, k);
+                mpz_sub_ui(k, k, 2);
+                // mpz_mod(k, k, Mp);
+
+                mpz_mod(l,k,Mp_2n);
+                mpz_tdiv_q_2exp(m,k,p);
+                mpz_add(k,l,m);
+                mpz_mod(k,k,Mp);
+
+                gmp_printf("%Zd", k);
+                cout << endl << "\n";
+
+                count++;
+            }
+            while (count<=limit);
+
+            if (mpz_cmp_ui(k, 0)==0) {
+                cout << "\nM_" << p << " is prime." << endl;
+            }
+            else {
+                cout << "\nM_" << p << " is composite." << endl;
+            }
+
+            mpz_clear(k);
+            mpz_clear(l);
+            mpz_clear(m);
+            mpz_clear(Mp);
+            mpz_clear(Mp_2n);
+
+            auto end_time = chrono::high_resolution_clock::now();
+            auto time = end_time-start_time;
+            results_Method_2[i][j] = static_cast<int>(chrono::duration_cast<chrono::microseconds>(time).count());
         }
     }
+    cout << "\nMethod 1:\n" << endl;
+
     for (int i = 0; i<mk; i++) {
         for (int j = 0; j<tk; j++) {
             cout << "\t" << results_Method_1[i][j];
         }
         cout << endl;
     }
+
+    cout << "\nMethod 2:\n" << endl;
 
     for (int i = 0; i<mk; i++) {
         for (int j = 0; j<tk; j++) {
