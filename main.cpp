17afff481ab9e7c2526093ec322a07accd3dc187
de-Manzanares <138251011+de-Manzanares@users.noreply.github.com>
2023-10-01 20:23:55 -0700

    testing modulo algorithm
    Method 1 uses the "straight" mod function
    Method 2 uses the Mersenne modulo algorithm

--- main.cpp
+++ main.cpp
@@ -14,10 +14,10 @@ int main()
     // cout << "p = ";
     // if ((cin >> p)&&(p>1)) {
 
-    int const mk = 20;
+    int const mk = 28;
     int const tk = 5;
 
-    int test_mp[mk] = {2, 3, 5, 7, 13, 17, 19, 31, 61, 89, 107, 127, 521, 607,1279, 2203, 2281, 3217, 4253, 4423};// 9689, 9941, 11213, 19937, 21701, 23209, 44497, 86243};//, 110503, 132049, 216091};
+    int test_mp[mk] = {2, 3, 5, 7, 13, 17, 19, 31, 61, 89, 107, 127, 521, 607,1279, 2203, 2281, 3217, 4253, 4423, 9689, 9941, 11213, 19937, 21701, 23209, 44497, 86243};//, 110503, 132049, 216091};
     int results_Method_1[mk][tk] = {0};
     int results_Method_2[mk][tk] = {0};
 
@@ -97,6 +97,9 @@ int main()
 
     for (int i=0; i<mk; i++) {
         for (int j=0; j<tk; j++){
+
+            p = test_mp[i];
+
             auto start_time = chrono::high_resolution_clock::now();
             mpz_t k;
             mpz_t l;
@@ -151,6 +154,7 @@ int main()
 
             auto end_time = chrono::high_resolution_clock::now();
             auto time = end_time-start_time;
+
             results_Method_2[i][j] = static_cast<int>(chrono::duration_cast<chrono::microseconds>(time).count());
         }
     }
